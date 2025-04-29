89df7eb9ad9bef2cde99220ca87e70aa100225f7
corentin5002 <csabier@orange.fr>
2025-04-29 03:03:47 +0200

    class for attack moving

--- kali-attacker/attack_functions.py
+++ kali-attacker/attack_functions.py
@@ -1,15 +1,20 @@
 import re
 import subprocess as sub
 
+# FILTER THIS IP ITS THE OWNER (myself)
+# 192.168.10.1
+
+
 def get_active_ips() -> list[str]:
     """
     Get active IPs in the network
     :return: list of active IPs
     """
 
-    command = ["docker", "exec", "kali-attacker", "bash", "-c"
-        , "nmap  -sn 192.168.10.0/24 --exclude 192.168.10.1,192.168.10.10 | grep 'Nmap scan report' | awk '{print $6}'"
-       ]
+    # command = ["docker", "exec", "kali-attacker", "bash", "-c"
+    #     , "nmap  -sn 192.168.10.0/24 --exclude 192.168.10.1,192.168.10.10 | grep 'Nmap scan report' | awk '{print $6}'"
+    #    ]
+    command = ["nmap  -sn 192.168.10.0/24 --exclude 192.168.10.1,192.168.10.10 | grep 'Nmap scan report' | awk '{print $6}'"]
 
     # Run the command
     ip_scan = sub.run(command
@@ -33,9 +38,10 @@ def get_active_ports(ip: str) -> list[str]:
     :return: list of active ports
     """
 
-    command = ["docker", "exec", "kali-attacker", "bash", "-c"
-        , f"nmap -sV -p 21,22,2121 {ip} | grep '^PORT' -A 100 | grep open | awk '{{print $1}}' | cut -d'/' -f1"
-       ]
+    # command = ["docker", "exec", "kali-attacker", "bash", "-c"
+    #     , f"nmap -sV -p 21,22,2121 {ip} | grep '^PORT' -A 100 | grep open | awk '{{print $1}}' | cut -d'/' -f1"
+    #    ]
+    command = [f"nmap -sV -p 21,22,2121 {ip} | grep '^PORT' -A 100 | grep open | awk '{{print $1}}' | cut -d'/' -f1"]
 
     # Run the command
     port_scan = sub.run(command
